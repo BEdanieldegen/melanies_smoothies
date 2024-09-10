da9dfe108192cd5fd5be12f7a39fdf1792411e20
BEdanieldegen <117719026+BEdanieldegen@users.noreply.github.com>
2024-09-10 17:10:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas
 from snowflake.snowpark.functions import col
 
 
@@ -17,6 +18,10 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
+pd_df = my_data_frame.to_pandas()
+st.datframe(pd_df)
+st.stop()
+
 
 name_on_order = st.text_input('Name of Smoothie')
 st.write('The Name of Your Smoothie will be:', name_on_order)
